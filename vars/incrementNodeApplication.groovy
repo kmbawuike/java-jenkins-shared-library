83bd655816b7471f58276a2b4fce2330070d1010
Mbawuike Kelechi <kmbawuike@gmail.com>
2025-12-01 19:56:07 -0500

    feat: updated library

--- vars/incrementNodeApplication.groovy
+++ vars/incrementNodeApplication.groovy
@@ -1,14 +1,15 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo 'Incrementing version'
-    sh "npm version minor --no-git-tag-version"
-
-    // read the updated version from the package.json file
-    def packageJson = readJSON file: 'package.json'
-    def version = packageJson.version
-
-    // set the new version as part of IMAGE_NAME
-    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
-
+  echo 'Incrementing version'
+  def version
+  def msg = sh(script: "git log -1 --pretty=%B", returnStdout: true).trim() // print out commit headline
+  if (msg.contains('BREAKING CHANGE')) {
+      version = sh(script: 'npm version major', returnStdout: true).trim().replace('v', '')
+  } else if (msg.startsWith('feat:')) {
+      version = sh(script: 'npm version minor -—no-git-tag-version', returnStdout: true).trim().replace('v', '')
+  } else {
+      version = sh(script: 'npm version patch', returnStdout: true).trim().replace('v', '')
+  }
+  env.IMAGE_NAME = "${version}-${env.BUILD_NUMBER}"
 }
